2407b67bd3e7053c68040f54709263405e3484d0
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-30 03:10:59 +0600

    api route

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("This is from express js server");
 });
-
+app.get("/api", (req, res) => {
+    res.send("This is from api endpoints");
+});
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
