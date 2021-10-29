773f6093845c1a4a1ccd43476b316cd7da7ce413
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-30 03:06:37 +0600

    little update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("This is from express js server");
